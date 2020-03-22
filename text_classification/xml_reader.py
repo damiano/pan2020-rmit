f5db75b69f104e71888e775285c78a496d885f59
xinhuanduan <s3713321@student.rmit.edu.au>
2020-03-22 20:08:29 +1100

    read twitter from usrl

--- text_classification/xml_reader.py
+++ text_classification/xml_reader.py
@@ -1,4 +1,16 @@
 from xml.dom.minidom import parse
 import xml.dom.minidom
 
-DOMTree = parse("data/en/0dwovd7nj6yg9m795ng2c629me0ccmrh.xml")
+
+
+def read_twitters(x):
+    s=""""""
+    DOMTree = parse("data/en/%s" % x)
+    collection =DOMTree.documentElement
+    documents = DOMTree.getElementsByTagName("document")
+    for document in documents:
+        s+=document.firstChild.wholeText
+    return s    
+
+s = read_twitters("0dwovd7nj6yg9m795ng2c629me0ccmrh.xml")  
+print(s)      
