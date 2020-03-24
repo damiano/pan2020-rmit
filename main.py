39252efa4d9328e081f33f3dd21c97025e713a52
xinhuanduan <s3713321@student.rmit.edu.au>
2020-03-24 22:43:29 +1100

    commit

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import spacy
 from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 from text_classification.predictors import predictors
 from text_classification.spacytokenizer import spacy_tokenizer
 from text_classification.dataset_creator import get_pandas
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
+from sklearn.metrics import confusion_matrix
 
 df_amazon = get_pandas()
 #bow_vector = CountVectorizer(tokenizer = spacy_tokenizer, ngram_range=(1,1))  
@@ -17,34 +19,35 @@ tfid_vector = TfidfVectorizer(tokenizer = spacy_tokenizer)
 X = df_amazon['twitters']
 ylabels =df_amazon['truth']
 
-X_train, X_test, y_train, y_test = train_test_split(X, ylabels, test_size=0.1,shuffle=False)
-#shuffle
-#cross validation 
-#ten fold validation 10pieces each of the loop 
-#300 instaces 
-#10-fold cross validation: 10 times 270 training, 30 test
-#leave-one-out: 300 times, 299 training, 1 test
-#extract the features
+kf = KFold(n_splits=10, random_state=None, shuffle=False)
+
+#X_train, X_test, y_train, y_test = train_test_split(X, ylabels, test_size=0.1,shuffle=False)
 
 classifier = LogisticRegression()
-#function much more complicated
-#try classifier
-
-# exrat 
-# weight the word
-# a couple of the slides
-
+#
 pipe = Pipeline([("cleaner", predictors()),
                 ('vectorizer',tfid_vector),
                 ('classifier', classifier)])
 
-pipe.fit(X_train,y_train)
+for train_index, test_index in kf.split(X):
+    X_train,X_test = X[train_index],X[test_index]
+    y_train,y_test= ylabels[train_index],ylabels[test_index]
+    pipe.fit(X_train,y_train)
+    predicted = pipe.predict(X_test)
+    print(confusion_matrix(y_test, predicted))
+    print(metrics.accuracy_score(y_test,predicted))
+    print(metrics.precision_score(y_test,predicted))
+    print(metrics.recall_score(y_test,predicted))
 
-predicted = pipe.predict(X_test)
+# pipe.fit(X_train,y_train)
 
-print(metrics.accuracy_score(y_test,predicted))
-print(metrics.precision_score(y_test,predicted))
-print(metrics.recall_score(y_test,predicted))
+# predicted = pipe.predict(X_test)
+
+
+
+# print(metrics.accuracy_score(y_test,predicted))
+# print(metrics.precision_score(y_test,predicted))
+# print(metrics.recall_score(y_test,predicted))
 
 
 #what is ngram_range
@@ -61,3 +64,31 @@ print(metrics.recall_score(y_test,predicted))
 
 # basic analysis(how many twitters of 0,1)
 # avaernumber of words in 
+
+#output confusion matrix(how many each class is predicted)
+#how many instaces, how many errors
+
+#shuffle
+#cross validation 
+#ten fold validation 10pieces each of the loop 
+#300 instaces 
+#10-fold cross validation: 10 times 270 training, 30 test
+#leave-one-out: 300 times, 299 training, 1 test
+#extract the features
+
+#function much more complicated
+#try classifier
+
+# exrat 
+# weight the word
+# a couple of the slides
+
+
+#1.bert
+#2.cross validation
+#3.confusion matrix
+#4.analysis
+#5.vectorization, try tf-idf
+#6.tf-idf combine with svm
+#7.special features(find features)
+#8.number of emojis
