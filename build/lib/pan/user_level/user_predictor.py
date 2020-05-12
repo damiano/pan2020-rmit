cbae5cb3e9497e5644143068f58ea976b7b050cd
O_o <48227836+duanxinhuan@users.noreply.github.com>
2020-05-13 06:24:52 +1100

    c

---
+++ build/lib/pan/user_level/user_predictor.py
@@ -0,0 +1,14 @@
+import pickle
+import numpy as np
+from tweet_level.tweetPredictor import tweet_predictor as t_predictor
+
+class user_predictor:
+    def __init__(self):
+        self.classifier = pickle.load(open('Data/classifier.txt','rb'))
+
+    def pridict_user(self,set):
+        
+    
+        result = self.classifier.predict(set)
+        return result
+    
--- parctice/pos.py
+++
@@ -1,8 +0,0 @@
-import en_core_web_sm
-
-nlp=en_core_web_sm.load()
-
-doc =nlp("All is well that ends well.")
-
-for word in doc:
-    print(word.text,word.pos_)
